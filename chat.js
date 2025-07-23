17ccf7aec6eefcee64d475e54d193457cdbf1caf
quimpositor <chris.j.quimpo.automation@gmail.com>
2025-07-23 13:25:24 +0800

    Update chat.js

--- chat.js
+++ chat.js
@@ -82,7 +82,7 @@
     const header = document.createElement('div');
     header.className = 'cbw-header';
     header.innerHTML = `
-        <img class="cbw-logo" src="https://your-logo-url-here" alt="Bot" />
+        <img class="cbw-logo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBVZ70UjLOJzXDzU6YrpBekSfMZsYpp8z-Ag&s" alt="Bot" />
         <div class="cbw-title">Your Bot</div>
         <button class="cbw-close">&times;</button>
     `;
